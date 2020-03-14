59894dace05825d577cf18b2cc366bda1f2f4668
方行 <fangxing1996@126.com>
2020-03-14 16:21:00 +0800

    Create passwordlist.h

---
+++ passwordlist.h
@@ -0,0 +1,25 @@
+#ifndef PASSWORDLIST_H
+#define PASSWORDLIST_H
+#include<vector>
+#include "passworditem.h"
+using namespace std;
+class PasswordList {
+public:
+    PasswordList();
+    void addItem(Passworditem& it);
+    void display(bool ismask = true);
+    void showTableHead();
+    void showTableContent(bool ismask = true);
+    void readFromFile();
+    void writeToFile(const string& filename);
+    void writeToDB();
+    void readFromDB();
+    Passworditem& operator[](int i); 
+    static void stringSplit(string& src, vector<string>& output, char c);
+    const vector<Passworditem>& getRecordList() const;
+    vector<Passworditem>& getRecordList() ;
+    vector<PasswordList>::size_type size();
+private:
+    vector<Passworditem>  recordList;
+};
+#endif
