080c2d48db947b9e7af5f735a2db19e43003b1dd
方行 <fangxing1996@126.com>
2020-03-14 16:24:11 +0800

    Create user.h

---
+++ user.h
@@ -0,0 +1,19 @@
+#ifndef USER_H
+#define USER_H
+#include<string>
+using namespace std;
+class User {
+public:
+    User();
+    void setName(const string& m);
+    void setLoginPassword(const string& p);
+    void getLoginMsgFromDB();
+    string getName();
+    string getLoginPassword();
+    void print();
+    void writeToDB();
+private:
+    string name;
+    string loginPassword;
+};
+#endif
