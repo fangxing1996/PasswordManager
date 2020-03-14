c5f5f0f8158df9ce136a245b118f84096c82855c
方行 <fangxing1996@126.com>
2020-03-14 16:22:57 +0800

    Create user.cpp

---
+++ user.cpp
@@ -0,0 +1,48 @@
+#include "user.h"
+#include "db.h"
+#include <iostream>
+using namespace std;
+User::User():name("fangxing"), loginPassword("root")
+{
+
+}
+
+void User::setName(const string& m)
+{
+    name = m;
+}
+void User::setLoginPassword(const string& p)
+{
+    loginPassword  = p;
+}
+
+void User::getLoginMsgFromDB()
+{
+    DB login;
+    login.attachDB();
+    login.readLoginMsg(*this);
+    login.closeDB();
+}
+
+void User::print()
+{
+    cout << name << " " << loginPassword << endl;
+}
+
+string User::getName()
+{
+    return name;
+}
+
+string User::getLoginPassword()
+{
+    return loginPassword;
+}
+
+void User::writeToDB()
+{
+    DB db;
+    db.attachDB();
+    db.updateUserMsg(*this);
+    db.closeDB();
+}
